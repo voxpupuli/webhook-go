a36a7d12ca1f3cbbf6a64bb42a31220a0b439e55
David Hollinger III <david.hollinger@moduletux.com>
2021-10-13 14:11:18 -0500

    Add basic authentication

---
+++ lib/users/users.go
@@ -0,0 +1,6 @@
+package users
+
+type Users struct {
+	User     string
+	Password string
+}
--- server/router.go
+++ server/router.go
@@ -3,6 +3,8 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	wapi "github.com/voxpupuli/webhook-go/api"
+	"github.com/voxpupuli/webhook-go/config"
+	"github.com/voxpupuli/webhook-go/lib/users"
 )
 
 // The NewRouter function sets up the main web routes
@@ -11,12 +13,16 @@ func NewRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	user := users.Users{
+		User:     config.GetConfig().Server.User,
+		Password: config.GetConfig().Server.Password,
+	}
 
 	health := new(wapi.HealthController)
 
 	router.GET("/health", health.Status)
 
-	api := router.Group("api")
+	api := router.Group("api", gin.BasicAuth(gin.Accounts{user.User: user.Password}))
 	{
 		v1 := api.Group("v1")
 		{
