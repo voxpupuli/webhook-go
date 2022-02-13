475787b0793e1629c68d71447e8a98fab3c11ba6
David Hollinger III <david.hollinger@moduletux.com>
2022-02-13 13:21:11 -0600

    Move bolt to orchestrators package and update conf

---
+++ lib/orchestrators/bolt.go
@@ -0,0 +1,98 @@
+package orchestrators
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Bolt struct {
+	Transport    *string
+	Targets      []string
+	Concurrency  *int64
+	RunAs        *string
+	SudoPassword *string
+	User         *string
+	Password     *string
+	HostKeyCheck *bool
+}
+
+func (b *Bolt) boltCommand(timeout time.Duration, command string) (*Result, error) {
+	cmd := []string{"bolt", "command", "run", "--targets"}
+
+	var targets string
+	for i := range b.Targets {
+		targets = targets + b.Targets[i] + ","
+	}
+	targets = strings.TrimSuffix(targets, ",")
+
+	if b.User != nil {
+		userArgs := []string{"-u", *b.User}
+		cmd = append(cmd, userArgs...)
+	}
+
+	if b.Password != nil {
+		passArgs := []string{"--password", *b.Password}
+		cmd = append(cmd, passArgs...)
+	}
+
+	if b.Transport != nil {
+		transArgs := []string{"--transport", *b.Transport}
+		cmd = append(cmd, transArgs...)
+	}
+
+	if b.Concurrency != nil {
+		concurrency := []string{"--concurrency", strconv.FormatInt(*b.Concurrency, 10)}
+		cmd = append(cmd, concurrency...)
+	}
+
+	if b.RunAs != nil {
+		runAs := []string{"--run-as", *b.RunAs}
+		cmd = append(cmd, runAs...)
+	}
+
+	if b.SudoPassword != nil {
+		sudoPass := []string{"--sudo-password", *b.SudoPassword}
+		cmd = append(cmd, sudoPass...)
+	}
+
+	if *b.HostKeyCheck {
+		cmd = append(cmd, "--no-host-key-check")
+	}
+
+	cmd = append(cmd, "--format", "json", "--connect-timeout", "120", command)
+
+	out, err := runCommand(strings.Join(cmd, " "), timeout)
+	if err != nil {
+		return nil, fmt.Errorf("Bolt: \"%s\": %s: %s", strings.Join(cmd, " "), out, err)
+	}
+
+	result := new(Result)
+	if err = json.Unmarshal(out, result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func runCommand(command string, timeout time.Duration) ([]byte, error) {
+	var args []string
+
+	if runtime.GOOS == "windows" {
+		args = []string{"cmd", "/C"}
+	} else {
+		args = []string{"/bin/sh", "-c"}
+	}
+	args = append(args, command)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	return cmd.Output()
+}
