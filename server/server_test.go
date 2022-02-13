d0b4735c43dcf71ccb9182f31506b6fe02b32784
David Hollinger III <david.hollinger@moduletux.com>
2022-02-13 13:21:50 -0600

    add server test

---
+++ server/server_test.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := NewRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/health", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"message\":\"running\"}", w.Body.String())
+}
