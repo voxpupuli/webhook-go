757b2252b3dcd0b543e8d239e8834987c919d04c
David Hollinger III <david.hollinger@moduletux.com>
2022-02-16 00:02:58 -0600

    Add orchestration logic to module controller and comments

--- api/module.go
+++ api/module.go
@@ -3,7 +3,6 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"os/exec"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -12,47 +11,89 @@ import (
 	"github.com/voxpupuli/webhook-go/lib/parsers"
 )
 
+// Module Controller
 type ModuleController struct{}
 
+// DeployModule takes int the current Gin context and parses the request
+// data into a variable then executes the r10k module deploy either through
+// an orchestrator defined in the orchestration library or a direct local execution
+// of the r10k deploy module command
 func (m ModuleController) DeployModule(c *gin.Context) {
-	data := parsers.Data{}
-	h := helpers.Helper{}
-	cmd := exec.Command("r10k", "deploy", "module")
-	conf := config.GetConfig().R10k
-	notify := config.GetConfig().ChatOps.Enabled
+	var data parsers.Data
+	var h helpers.Helper
+
+	// Set the base r10k command into a string slice
+	cmd := []string{"r10k", "deploy", "module"}
+
+	// Get the configuration
+	conf := config.GetConfig()
+
+	// Setup chatops connection so we don't have to repeat the process
 	conn := chatopsSetup()
 
+	// Parse the data from the request and error if parsing fails
 	err := data.ParseData(c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error Parsing Webhook", "error": err})
 		c.Abort()
-		if notify {
+		if conf.ChatOps.Enabled {
 			conn.PostMessage(http.StatusInternalServerError, "Error Parsing Webhook")
 		}
 		return
 	}
 
-	cmd.Args = append(cmd.Args, data.ModuleName)
-	cmd.Args = append(cmd.Args, fmt.Sprintf("--config=%s", h.GetR10kConfig()))
+	// Append module name and r10k configuration to the cmd string slice
+	cmd = append(cmd, data.ModuleName)
+	cmd = append(cmd, fmt.Sprintf("--config=%s", h.GetR10kConfig()))
 
-	if conf.Verbose {
-		cmd.Args = append(cmd.Args, "-v")
+	// Set additional optional r10k flags if they are set
+	if conf.R10k.Verbose {
+		cmd = append(cmd, "-v")
 	}
 
-	res, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Errorf("cmd.Run() failed with error %s", string(res))
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "error executing command", "error": string(res)})
-		c.Abort()
-		if notify {
-			conn.PostMessage(http.StatusInternalServerError, data.ModuleName)
+	// Determine if orchestration is enabled and either pass the cmd string slice to a
+	// the orchestrationExec function or localExec function.
+	// On an error this will:
+	//		* Log the error, orchestration type, and command
+	//		* Respond with an HTTP 500 error and return the command result in JSON format
+	//		* Abort the request
+	//		* Notify ChatOps service if enabled
+	//
+	// On success this will:
+	//		* Respond with an HTTP 202 and the result in JSON format
+	if conf.Orchestration.Enabled {
+		res, err := orchestrationExec(cmd)
+		if err != nil {
+			// TODO: Replace with proper custom error types
+			log.Errorf("orchestrator `%s` failed to execute command `%s` with error: `%s`", *conf.Orchestration.Type, cmd, err)
+			c.JSON(http.StatusInternalServerError, res)
+			c.Abort()
+			if conf.ChatOps.Enabled {
+				conn.PostMessage(http.StatusInternalServerError, data.ModuleName)
+			}
+			return
 		}
-		return
-	}
+		c.JSON(http.StatusAccepted, res)
+		if conf.ChatOps.Enabled {
+			conn.PostMessage(http.StatusAccepted, data.ModuleName)
+		}
+	} else {
+		res, err := localExec(cmd)
+		if err != nil {
+			log.Errorf("cmd.Run() failed with error %s", string(res))
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "error executing command", "error": string(res)})
+			c.Abort()
+			if conf.ChatOps.Enabled {
+				conn.PostMessage(http.StatusInternalServerError, data.ModuleName)
+			}
+			return
+		}
 
-	c.JSON(http.StatusAccepted, gin.H{"message": string(res)})
-	log.Info(fmt.Sprintf("\n%s", string(res)))
-	if notify {
-		conn.PostMessage(http.StatusAccepted, data.ModuleName)
+		c.JSON(http.StatusAccepted, gin.H{"message": string(res)})
+		log.Info(fmt.Sprintf("\n%s", string(res)))
+		if conf.ChatOps.Enabled {
+			conn.PostMessage(http.StatusAccepted, data.ModuleName)
+		}
+
 	}
 }
