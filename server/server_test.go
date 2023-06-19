85fab11b26b83909958acb24837b9c2937bd6213
Sebastian Rakel <sebastian@devunit.eu>
2023-06-19 22:07:27 +0200

    Add test with github payload

--- server/server_test.go
+++ server/server_test.go
@@ -1,11 +1,15 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/voxpupuli/webhook-go/config"
+	"github.com/voxpupuli/webhook-go/lib/queue"
 )
 
 func TestPingRoute(t *testing.T) {
@@ -17,4 +21,34 @@ func TestPingRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "{\"message\":\"running\"}", w.Body.String())
+}
+
+func TestQueue(t *testing.T) {
+	mCfg := "../lib/helpers/yaml/webhook.queue.yaml"
+	config.Init(&mCfg)
+
+	queue.Work()
+
+	router := NewRouter()
+
+	payloadFile, err := os.Open("../lib/parsers/json/github/push.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/api/v1/r10k/environment", payloadFile)
+	req.Header.Add("X-GitHub-Event", "push")
+
+	router.ServeHTTP(w, req)
+
+	var queueItem queue.QueueItem
+	err = json.Unmarshal(w.Body.Bytes(), &queueItem)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 202, w.Code)
+	assert.Equal(t, "simple-tag", queueItem.Name)
+	assert.Equal(t, "added", queueItem.State)
 }
