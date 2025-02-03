1fff9335942ad276b50a7fc74548e2e04eec3a33
David Hollinger <david.hollinger@moduletux.com>
2025-02-03 16:39:49 -0600

    add gitea delete event

--- lib/parsers/gitea.go
+++ lib/parsers/gitea.go
@@ -1,6 +1,7 @@
 package parsers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,7 +16,7 @@ func giteaWebhookType(r *http.Request) string {
 }
 
 // parseGitea processes a Gitea webhook, extracting branch, repository, and user information.
-// Handles "push" events to set relevant fields based on the payload.
+// Handles "push" and "delete" events to set relevant fields based on the payload.
 func (d *Data) parseGitea(c *gin.Context) error {
 	payload, err := io.ReadAll(c.Request.Body)
 	if err != nil {
@@ -38,8 +39,38 @@ func (d *Data) parseGitea(c *gin.Context) error {
 		d.RepoUser = e.Repo.Owner.UserName
 		d.Completed = true
 		d.Succeed = true
+	case "delete":
+		e, err := parseDeleteHook(payload)
+		if err != nil {
+			return api.ErrInvalidReceiveHook
+		}
+		d.Branch = e.Ref
+		d.Deleted = true
+		d.ModuleName = e.Repo.Name
+		d.RepoName = e.Repo.FullName
+		d.RepoUser = e.Repo.Owner.UserName
+		d.Completed = true
+		d.Succeed = true
 	default:
 		return fmt.Errorf("unknown event type %s", eventType)
 	}
 	return nil
+}
+
+// This function parses a Gitea delete event into a struct of type
+// api.DeletePayload for use later.
+func parseDeleteHook(raw []byte) (*api.DeletePayload, error) {
+	hook := new(api.DeletePayload)
+	if err := json.Unmarshal(raw, hook); err != nil {
+		return nil, err
+	}
+
+	switch {
+	case hook.Repo == nil:
+		return nil, api.ErrInvalidReceiveHook
+	case len(hook.Ref) == 0:
+		return nil, api.ErrInvalidReceiveHook
+	}
+
+	return hook, nil
 }
